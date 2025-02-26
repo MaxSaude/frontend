f6deed044fb06072a2d1728ea1b25330e757097b
Gabriel Moretti Woicolesco <114514587+GabMoretti@users.noreply.github.com>
2025-02-25 21:05:59 -0300

    page agendamentos iniciada

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import EmpresaInterface from "./pages/Empresas/Inicio";
 import Home from "./pages/Home";
 import Dashboard from "./components/Dashboard";
+import AgendamentosInterface from "./pages/Agendamentos";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route element={<Dashboard />}>
         <Route path="/" element={<Home />} />
         <Route path="/empresas" element={<EmpresaInterface />} />
+        <Route path="/agendamentos" element={<AgendamentosInterface />} />
       </Route>
     </Routes>
   );
