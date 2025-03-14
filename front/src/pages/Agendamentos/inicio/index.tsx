e455fc93f62e8d8d457429c40817165edb6c04e3
Gabriel Moretti Woicolesco <114514587+GabMoretti@users.noreply.github.com>
2025-03-13 21:25:06 -0300

    tela arrumada mas nao finalizada

--- front/src/pages/Agendamentos/inicio/index.tsx
+++ front/src/pages/Agendamentos/inicio/index.tsx
@@ -5,53 +5,6 @@ import imagemTarde from "./Cloud-Sun-4--Streamline-Solar.png";
 
 const AgendamentosInterface: React.FC = () => {
 
-    const [agendamentoList, setEmpresaList] = useState<Agendamento[]>([])
-        const [agendamentoAtual, setAgendamentoAtual] = useState<Agendamento | null>(null)
-        const {isOpen, onOpen, onClose} = useDisclosure();
-    
-        useEffect(() =>{
-    
-            const fetchData = async () => {
-                const response = await listarTodosAgendamento();
-                setAgendamentoList(response.data)
-            }
-    
-            fetchData();
-    
-        }, [])
-
-    const cadastrarAgendamento = () =>{
-        setAgendamentoAtual(null)
-        onOpen()
-    }
-
-    const deletarAgendamento = async (codigo: string)=>{
-    
-            const confirmDelete = window.confirm("Você tem certeza que deseja excluir este agendamento?");
-            
-            if (confirmDelete) {
-                try {
-                    await deletarAgendamentoAPI(codigo)
-                    setAgendamentoList(agendamentoList.filter(agendamento => agendamento.codigo != codigo))
-    
-                    alert("Excluido com sucesso !")
-    
-                } catch (error) {
-                    alert("Agendamento Possui ligação com outro tabela, não pode excluir !")
-                }
-            }
-        }
-    
-        const handleCloseModal=()=>{
-            onClose()
-            setAgendamentoAtual(null)
-        }
-    
-        const editarAgendamento = (agendamento : Agendamento) =>{
-            setAgendamentoAtual(agendamento)
-            onOpen()
-        }
-
     return(
         <div style={{ backgroundColor: '#A8A8A8', width: '100%', height: '100vh' }}>
             <div className={styles.tittle}>Agendamentos</div>
@@ -59,7 +12,7 @@ const AgendamentosInterface: React.FC = () => {
             <div className={styles.alinhamentoCalenButt}>
                 <div>calendario</div>
 
-                <button className={styles.buttonAgendamento} style={{ marginLeft: '100px'}} onClick={cadastrarAgendamento}>NOVO AGENDAMENTO</button>
+                <button className={styles.buttonAgendamento} style={{ marginLeft: '100px'}}>NOVO AGENDAMENTO</button>
             </div>
 
             <div className={styles.boxListaAgendamentos}>
